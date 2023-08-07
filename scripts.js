66b649f31ce1dbfd5027ef7192ad3b9e9c000f5e
cdprojects <caseydentprojects@gmail.com>
2023-08-07 16:49:18 +0300

    Incon-hamburger bar added

--- scripts.js
+++ scripts.js
@@ -1,12 +1,7 @@
 function toggleMenu() {
-    const subMenus = document.querySelector('.sub-menus');
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-  
-    subMenus.style.display = subMenus.style.display === 'none' || subMenus.style.display === '' ? 'block' : 'none';
-  
-    // Toggle between hamburger and 'X' icon
-    hamburgerMenu.innerHTML = subMenus.style.display === 'block' ?
-      '<div class="bar" style="transform: rotate(45deg);"></div><div class="bar" style="opacity: 0;"></div><div class="bar" style="transform: rotate(-45deg);"></div>' :
-      '<div class="bar"></div><div class="bar"></div><div class="bar"></div>';
-  }
-  
+  const subMenus = document.querySelector('.sub-menus');
+  const hamburgerMenu = document.querySelector('.icon-burger');
+
+  subMenus.style.display = subMenus.style.display === 'none' || subMenus.style.display === '' ? 'block' : 'none';
+  hamburgerMenu.classList.toggle('active');
+}
