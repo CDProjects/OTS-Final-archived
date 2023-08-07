ec9055a8fede8382353bfabba9d635a5c5b8e1b9
cdprojects <caseydentprojects@gmail.com>
2023-08-07 17:09:02 +0300

    Sub menus adjusted

--- scripts.js
+++ scripts.js
@@ -2,6 +2,15 @@ function toggleMenu() {
   const subMenus = document.querySelector('.sub-menus');
   const hamburgerMenu = document.querySelector('.icon-burger');
 
-  subMenus.style.display = subMenus.style.display === 'none' || subMenus.style.display === '' ? 'block' : 'none';
+  if (subMenus.style.maxHeight === '0px' || subMenus.style.maxHeight === '') {
+    // Ease-in effect when opening
+    subMenus.style.transition = 'max-height 1s ease-in'; // 1-second ease-in
+    subMenus.style.maxHeight = '100vh';
+  } else {
+    // Immediate disappearance when closing
+    subMenus.style.transition = 'none'; // No transition
+    subMenus.style.maxHeight = '0px';
+  }
+
   hamburgerMenu.classList.toggle('active');
 }
