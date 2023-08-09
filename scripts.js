6ef99393096d960d567cfeaf01bef4aec7b2b4f2
cdprojects <caseydentprojects@gmail.com>
2023-08-09 12:05:21 +0300

    Contact scroll sub menu added

--- scripts.js
+++ scripts.js
@@ -1,44 +1,51 @@
 function toggleMenu() {
-  const subMenus = document.querySelector('.sub-menus');
-  const hamburgerMenu = document.querySelector('.icon-burger');
+  const subMenus = document.querySelector(".sub-menus");
+  const hamburgerMenu = document.querySelector(".icon-burger");
 
-  if (subMenus.style.maxHeight === '0px' || subMenus.style.maxHeight === '') {
+  if (subMenus.style.maxHeight === "0px" || subMenus.style.maxHeight === "") {
     // Ease-in effect when opening
-    subMenus.style.transition = 'max-height 0.5s ease-in'; // 1-second ease-in
-    subMenus.style.maxHeight = '100vh';
+    subMenus.style.transition = "max-height 0.5s ease-in"; // 1-second ease-in
+    subMenus.style.maxHeight = "100vh";
   } else {
     // Immediate disappearance when closing
-    subMenus.style.transition = 'none'; // No transition
-    subMenus.style.maxHeight = '0px';
+    subMenus.style.transition = "none"; // No transition
+    subMenus.style.maxHeight = "0px";
   }
 
-  hamburgerMenu.classList.toggle('active');
+  hamburgerMenu.classList.toggle("active");
 }
-
-document.getElementById('home-link').addEventListener('click', function(e) {
+ // HOME sub menu scroll to bottom of page
+document.getElementById("home-link").addEventListener("click", function (e) {
   e.preventDefault(); // Prevent the default behavior
   window.scrollTo(0, 0); // Scroll to the top of the page
 });
 
-document.addEventListener('DOMContentLoaded', () => {
+  // CONTACT sub menu scroll to bottom of page
+  document.getElementById('contact-link').addEventListener('click', function(e) {
+    e.preventDefault();
+    var contactSection = document.getElementById('contact-section');
+    contactSection.scrollIntoView({ behavior: 'smooth' });
+  });
+  
+document.addEventListener("DOMContentLoaded", () => {
   // Preload images
-  const images = ['Images/Akseli Peltoniemi.png', 'Images/Alejandro Ruiz.png'];
+  const images = ["Images/Akseli Peltoniemi.png", "Images/Alejandro Ruiz.png"];
   images.forEach((src) => {
     const img = new Image();
     img.src = src;
   });
 
   // Carousel functionality
-  const carouselItems = document.querySelectorAll('.carousel-item');
+  const carouselItems = document.querySelectorAll(".carousel-item");
   let currentIndex = 0;
 
   function carouselCycle() {
-    carouselItems[currentIndex].classList.remove('active');
+    carouselItems[currentIndex].classList.remove("active");
     currentIndex = (currentIndex + 1) % carouselItems.length;
-    carouselItems[currentIndex].classList.add('active');
+    carouselItems[currentIndex].classList.add("active");
   }
 
   // Cycle through images every 3 seconds
   setInterval(carouselCycle, 3000);
-});
 
+});
