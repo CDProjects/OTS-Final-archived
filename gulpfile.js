0c12cb31477a40a4e3f69b7933d1b540303de48f
caseydent <caseydentprojects@gmail.com>
2023-09-21 11:30:52 +0300

    Amended gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task('minify-html', function() {
 //Task to add images to dist
 gulp.task('copy-images', function() {
     return gulp.src('./Images/**/*')
-        .pipe(gulp.dest('./dist/Images'));
+        .pipe(gulp.dest('./dist/images'));
 });
 
 // Default task that runs all the above tasks
